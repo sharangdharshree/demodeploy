2540021e48d42ef723e6128ea4d7fe4a1b19db50
Sharangdhar <sharangdhar7984@gmail.com>
2024-07-10 09:43:38 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/twitter", (req, res) => {
+  res.send("sharangdhar7984");
+});
+
+app.get("/login", (req, res) => {
+  res.send("use your credentials ");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`);
+});
